69a2f926c91b55b74d0fd7d0b204113ce54361ac
MH Ghatee <mhghatee@example.com>
2026-09-07 13:02:49 +0330

    Use working assets path for new Iran map image

--- sync_all.php
+++ sync_all.php
@@ -4,7 +4,7 @@ $files = [
     "loader_concepts.html",
     "index.html",
     "assets/iran_padel_hud_map_v2.jpg",
-    "iran_padel_hud_map_v3.jpg",
+    "assets/silveriom_network_map_kish.jpg",
     "assets/iran_padel_hud_map_v2.webp",
     "homepage.html",
     "media-planner.html",
